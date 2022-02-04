48f8ae114187fb8b0b5d8542d62ca3ca27f55fab
dpalmasan <dpalmasan@gmail.com>
2022-02-04 10:01:10 -0300

    Add Hellos function to process names

--- hello/hello.go
+++ hello/hello.go
@@ -14,8 +14,10 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
 	// Request a greeting message.
-	message, err := greetings.Hello("Gladys")
+	messages, err := greetings.Hellos(names)
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
@@ -24,5 +26,5 @@ func main() {
 
 	// If no error was returned, print the returned message
 	// to the console.
-	fmt.Println(message)
+	fmt.Println(messages)
 }
